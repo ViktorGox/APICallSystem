50aeb5b2a224d4b0e2f1305636642872de246960
VitkorKrGo <viktor.krastev03@gmail.com>
2024-04-02 11:47:33 +0200

    Added APICall class which will be the point where the Http requests are executed, aslo added UrlFactory, which will be responsible for making sure urls are correct.

---
+++ APICallSystem/API/APICall.cs
@@ -0,0 +1,66 @@
+﻿using CustomConsole;
+using System.Text;
+
+namespace APICallSystem.API
+{
+    public static class APICall
+    {
+        public static async Task<string> Get(string url)
+        {
+            using HttpClient client = new HttpClient();
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    CConsole.WriteLine("Response: " + responseBody);
+                    return responseBody;
+                }
+                else
+                {
+                    CConsole.WriteLine("Request failed with status code " + response.StatusCode);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                CConsole.WriteLine("Request failed: " + e.Message);
+            }
+            return "";
+        }
+
+
+
+        public static async Task Method()
+        {
+            using HttpClient client = new();
+            try
+            {
+                // Create a JSON string for the request body
+                string jsonBody = "{\"label\": \"value\"}";
+
+                // Create the HttpContent object with JSON content
+                HttpContent content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+                // Send the POST request with the specified body
+                HttpResponseMessage response = await client.PostAsync("https://localhost:7225/api/Module", content);
+                // Check if the response is successful
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    CConsole.WriteLine("Response: " + responseBody);
+                }
+                else
+                {
+                    CConsole.WriteLine("Request failed with status code " + response.StatusCode);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                CConsole.WriteLine("Request failed: " + e.Message);
+            }
+        }
+    }
+}
---
+++ APICallSystem/API/UrlFactory.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace APICallSystem.API
+{
+    internal static class UrlFactory
+    {
+        public static string? Last { get; set; }
+        private static readonly StringBuilder current = new();
+
+        public static string Create(bool isHttps, string mainUrl, ushort port = 0, string version = "")
+        {
+            current.Clear();
+            Http(isHttps);
+            MainUrl(mainUrl);
+            Port(port);
+            Path(version);
+            current.Append('/');
+            Last = current.ToString();
+            return Last;
+        }
+
+        private static void Http(bool isHttps)
+        {
+            if (isHttps)
+            {
+                current.Append("https");
+            }
+            else
+            {
+                current.Append("http");
+            }
+
+            current.Append("://");
+        }
+
+        private static void MainUrl(string mainUrl)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(mainUrl);
+            current.Append(mainUrl);
+        }
+
+        private static void Port(ushort port)
+        {
+            if (port == 0) return;
+            current.Append(':');
+            current.Append(port);
+        }
+
+        private static void Path(string version)
+        {
+            current.Append('/');
+            current.Append(version);
+        }
+
+        public static bool Validate()
+        {
+            // Validation
+            return true;
+        }
+    }
+}
