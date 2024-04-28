c92a86ad15c4ff8401bfb2a659aeb4da7077aa7c
VitkorKrGo <viktor.krastev03@gmail.com>
2024-04-29 00:19:35 +0300

    Added post to the entity class, and changed the old code to use the new api call classes.

--- APICallSystem/EntityContext/Entity.cs
+++ APICallSystem/EntityContext/Entity.cs
@@ -1,4 +1,5 @@
 ﻿using APICallSystem.API;
+using APICallSystem.API.APICalls;
 using APICallSystem.API.EventArguments;
 using APICallSystem.DataAdaptation;
 
@@ -7,7 +8,7 @@ namespace APICallSystem.EntityContext
     /// <summary>
     /// Represents entity present in the back end, allows for calling available end points.
     /// </summary>
-    internal class Entity<T>(string endPoint, string baseUrl, IHttpReqResponseAdapter responseAdapter, IHttpReqBodyAdapter bodyAdapter)
+    internal class Entity<T>(string endPoint, string baseUrl, IHttpReqResponseAdapter responseAdapter, IHttpReqBodyAdapter bodyAdapter) where T : class
     {
         private readonly string _endPoint = endPoint;
         private readonly string _baseUrl = baseUrl;
@@ -23,8 +24,17 @@ namespace APICallSystem.EntityContext
         {
             Task.Run(async () =>
             {
-                APICall<T> apiCall = new(RequestType.Get, _baseUrl + _endPoint + "/" + id, _responseAdapter, OnSuccess: onSuccess, OnFailure: onFailure);
-                await apiCall.Execute();
+                GetAPICall<T> getApiCall = new(_baseUrl + _endPoint + "/" + id, _responseAdapter, onSuccess, onFailure);
+                await getApiCall.Execute();
+            });
+        }
+
+        public void Post(T body, Action<OnRequestSuccessEventArgs<T>>? onSuccess = null, Action<OnRequestFailureEventArgs>? onFailure = null) 
+        {
+            Task.Run(async () =>
+            {
+                PostAPICall<T> postApiCall = new(_baseUrl + _endPoint, _responseAdapter, _bodyAdapter, body, onSuccess, onFailure);
+                await postApiCall.Execute();
             });
         }
     }
