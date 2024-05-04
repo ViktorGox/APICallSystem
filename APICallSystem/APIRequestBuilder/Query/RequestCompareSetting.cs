c0fb679918dae074d50c381190915c6c04ae6538
VitkorKrGo <viktor.krastev03@gmail.com>
2024-05-04 17:37:06 +0300

    Added enum for the possible compare settings, currently only contains 4 + None. More will be added later.

---
+++ APICallSystem/APIRequestBuilder/Query/RequestCompareSetting.cs
@@ -0,0 +1,11 @@
+﻿namespace APICallSystem.APIRequestBuilder.Query
+{
+    internal enum RequestCompareSetting
+    {
+        None,
+        Equals,
+        NotEquals,
+        LessThan,
+        MoreThan
+    }
+}
