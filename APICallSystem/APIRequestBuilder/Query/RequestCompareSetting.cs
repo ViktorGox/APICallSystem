106df8b47e94dd098dfc050a05995972bd324368
VitkorKrGo <viktor.krastev03@gmail.com>
2024-05-05 01:22:11 +0300

    Added more setting options in the enum.

--- APICallSystem/APIRequestBuilder/Query/RequestCompareSetting.cs
+++ APICallSystem/APIRequestBuilder/Query/RequestCompareSetting.cs
@@ -3,9 +3,33 @@
     internal enum RequestCompareSetting
     {
         None,
-        Equals,
-        NotEquals,
-        LessThan,
-        MoreThan
+
+        Equals, // =
+        NotEquals, // !=
+        EqualsCS, // ILIKE
+
+        IncludesAny, // ILIKE '%a%'
+        NotIncludesAny, // NOT ILIKE '%a%'
+        IncludesAnyCS, // "LIKE '%a%'
+        NotIncludesAnyCS, // NOT LIKE '%a%'
+
+        IncludesAnyStart, // ILIKE 'a%'
+        NotIncludesAnyStart, // NOT ILIKE 'a%'
+        IncludesAnyStartCS, // LIKE 'a%'
+        NotIncludesAnyStartCS, // NOT LIKE 'a%'
+
+        IncludesAnyEnd, // ILIKE '%a'
+        NotIncludesAnyEnd, // NOT ILIKE '%a'
+        IncludesAnyEndCS, // LIKE '%a'
+        NotIncludesAnyEndCS, // NOT LIKE '%a'
+
+        MoreThan, // >
+        LessThan, // <
+        MoreThanOrEqualsTo, // >=
+        LessThanOrEqualsTo, // <=
+
+        IsNull, // IS NULL
+        Between, // BETWEEN x AND y
+        NotBetween, // NOT BETWEEN x AND y
     }
 }
