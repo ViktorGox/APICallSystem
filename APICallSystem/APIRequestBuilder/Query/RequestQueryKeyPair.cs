d38ceaeba8efabd4909d1c5a72385749e148b068
VitkorKrGo <viktor.krastev03@gmail.com>
2024-05-04 17:37:29 +0300

    Added class which will be used as key for a dictionary for the request building.

---
+++ APICallSystem/APIRequestBuilder/Query/RequestQueryKeyPair.cs
@@ -0,0 +1,26 @@
+﻿namespace APICallSystem.APIRequestBuilder.Query
+{
+    /// <summary>
+    /// Represents key for the dictionary used in <see cref="RequestBuilder"/> for the query data. 
+    /// Instances with the same parameter name and setting will return true when compared.
+    /// </summary>
+    /// <param name="setting">The setting associated with the variable. Defaults to <see cref="RequestCompareSetting.None"/>.</param>
+    /// <param name="parameterName">Name of the variable/property of the represented model class.</param>
+    internal sealed class RequestQueryKeyPair(string parameterName, RequestCompareSetting setting = RequestCompareSetting.None)
+    {
+        public string ParameterName { get; private set; } = parameterName;
+        public RequestCompareSetting Setting { get; private set; } = setting;
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ParameterName, Setting);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+            RequestQueryKeyPair other = (RequestQueryKeyPair)obj;
+            return ParameterName == other.ParameterName && Setting == other.Setting;
+        }
+    }
+}
